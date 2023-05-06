c5d01d66d99eecde78643734b21e3577ba89176a
Elliott Mangham <elliott@coderesolution.com>
2023-05-06 00:37:01 +0000

    Init files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": 9,
+		"sourceType": "module"
+	},
+	"rules": {
+	},
+	"globals": {
+		"gsap": "readonly",
+		"ScrollTrigger": "readonly"
+	}
+}
