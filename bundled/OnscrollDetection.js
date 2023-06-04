d75a5a2ec7ab13005596b2a3e3129db1535953f8
Elliott Mangham <elliott@coderesolution.com>
2023-06-04 22:59:29 +0100

    Fix trigger attr error

--- bundled/OnscrollDetection.js
+++ bundled/OnscrollDetection.js
@@ -78,8 +78,14 @@
         // If data-onscroll-auto is present and data-onscroll-trigger is not, use the parent element as the trigger
         return element.parentElement;
       } else if (element.hasAttribute('data-onscroll-trigger')) {
-        // If data-onscroll-trigger is present, use the parent element as the trigger
-        return element.parentElement;
+        // If data-onscroll-trigger is present, try to find the DOM element specified by the attribute
+        var triggerElement = document.querySelector(element.dataset.onscrollTrigger);
+        if (triggerElement) {
+          return triggerElement;
+        } else {
+          console.error("Element specified by data-onscroll-trigger not found: " + element.dataset.onscrollTrigger);
+          return element;
+        }
       } else {
         // Otherwise, use the element itself as the trigger
         return element;
